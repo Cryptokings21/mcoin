df0dbf5d714423913e718562ca73ad1355bb7dc3
Cryptokings21 <87268234+Cryptokings21@users.noreply.github.com>
2021-07-12 19:19:28 +0530

    Create mcon.sh

---
+++ mcon.sh
@@ -0,0 +1,10 @@
+# monero xmr mining
+
+sudo apt get update
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev 
+git clone https://github.com/xmrig/xmrig.git
+ mkdir xmrig/build 
+ cd xmrig/build
+  cmake .. 
+  make -j$(nproc) 
+  ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:86jHEQZv6NVRjmWB7dzmqUDU2U9nozok8L7Vfz2JqHBsc8ndgwKPsxhA68DMmQRUGUMwmNX3PMrfWWADTvkMPkmaJh77R1V.colab7
