7ffb5b72542f786b34e85721a15be9d17c2eab0e
Cryptokings21 <87268234+Cryptokings21@users.noreply.github.com>
2021-07-12 19:25:56 +0530

    Create xmrlol.sh

---
+++ xmrlol.sh
@@ -0,0 +1,6 @@
+# xmr mining lolminer
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz 
+cd 1.29
+./lolMiner --algo ETHASH --pool us.unminable.com:3333 --user XMR:86jHEQZv6NVRjmWB7dzmqUDU2U9nozok8L7Vfz2JqHBsc8ndgwKPsxhA68DMmQRUGUMwmNX3PMrfWWADTvkMPkmaJh77R1V.prashanthn21 --ethstratum ETHPROXY
+
